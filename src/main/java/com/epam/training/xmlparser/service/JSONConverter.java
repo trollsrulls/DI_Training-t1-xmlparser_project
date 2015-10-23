6ef4286c9a55599374be96e842b544eca5f0c335
trollsrulls <maxim131994@gmail.com>
2015-10-23 11:59:10 +0300

    updated

--- src/main/java/com/epam/training/xmlparser/service/JSONConverter.java
+++ src/main/java/com/epam/training/xmlparser/service/JSONConverter.java
@@ -14,11 +14,11 @@ public class JSONConverter {
 
     public JSONObject parseDocument(Document doc) {
         JSONObject root = new JSONObject();
-        visitNode(doc, root);
+        parseNode(doc, root);
         return root;
     }
 
-    private void visitNode(Node node, JSONObject jsonObject) {
+    private void putAttributesToJsonObject(Node node, JSONObject jsonObject) {
         if (node instanceof Element) {
             NamedNodeMap attrs = node.getAttributes();
             for (int i = 0; i < attrs.getLength(); i++) {
@@ -27,48 +27,59 @@ public class JSONConverter {
                 jsonObject.put(key, value);
             }
         }
+    }
 
+    private List<Node> extractChildNodes(Node node) {
         NodeList children = node.getChildNodes();
         List<Node> childNodes = new ArrayList<>();
         for (int i = 0; i < children.getLength(); i++) {
             childNodes.add(children.item(i));
         }
-        Collections.sort(childNodes, (o1, o2) -> o1.getNodeName().compareTo(o2.getNodeName()));
+        return childNodes;
+    }
 
+    private void fillJsonObjectLevel(List<Node> childNodes, JSONObject jsonObject) {
         for (int i = 0; i < childNodes.size(); i++) {
             int start = i;
             for (int j = i + 1; j < childNodes.size(); j++) {
-                if (childNodes.get(i).getNodeName().equals(childNodes.get(j).getNodeName())) {
-                    i = j;
+                if (!childNodes.get(i).getNodeName()
+                        .equals(childNodes.get(j).getNodeName())) {
+                    i = j - 1;
+                    break;
                 }
             }
             int end = i;
 
-            if (end - start == 0) {
-                Node currentChild = childNodes.get(end);
+            Node currentChild = childNodes.get(start);
 
-                if (JSONValue.TEXT_KEY.equals(currentChild.getNodeName())
-                        && StringUtils.isNotBlank(currentChild.getNodeValue())) {
+            if (JSONValue.TEXT_KEY.equals(currentChild.getNodeName())) {
+                if (end - start == 0 && StringUtils.isNotBlank(currentChild.getNodeValue())) {
                     JSONValue value = new JSONValue(currentChild.getNodeValue());
                     jsonObject.put(JSONValue.TEXT_KEY, value);
-                    continue;
                 }
-                JSONObject childJsonObject = new JSONObject();
-                jsonObject.put(currentChild.getNodeName(), childJsonObject);
-                visitNode(currentChild, childJsonObject);
             } else {
-                if (JSONValue.TEXT_KEY.equals(childNodes.get(start).getNodeName())) {
-                    continue;
-                }
-                JSONArray childJsonArray = new JSONArray();
-                jsonObject.put(childNodes.get(start).getNodeName(), childJsonArray);
-                for (int j = start; j <= end; j++) {
-                    JSONObject arrayObject = new JSONObject();
-                    childJsonArray.put(arrayObject);
-                    visitNode(childNodes.get(j), arrayObject);
+                if (end - start == 0) {
+                    JSONObject childJsonObject = new JSONObject();
+                    jsonObject.put(currentChild.getNodeName(), childJsonObject);
+                    parseNode(currentChild, childJsonObject);
+                } else {
+                    JSONArray childJsonArray = new JSONArray();
+                    jsonObject.put(currentChild.getNodeName(), childJsonArray);
+                    for (int j = start; j <= end; j++) {
+                        JSONObject arrayObject = new JSONObject();
+                        childJsonArray.put(arrayObject);
+                        parseNode(childNodes.get(j), arrayObject);
+                    }
                 }
             }
         }
+    }
+
+    private void parseNode(Node node, JSONObject jsonObject) {
+        putAttributesToJsonObject(node, jsonObject);
+        List<Node> childNodes = extractChildNodes(node);
+        Collections.sort(childNodes, (o1, o2) -> o1.getNodeName().compareTo(o2.getNodeName()));
+        fillJsonObjectLevel(childNodes, jsonObject);
     }
 
 }
